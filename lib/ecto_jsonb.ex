97ac88d31b35621e0d493f6eb6b36fd90ab8c218
ontofractal <ontofractal@protonmail.com>
2018-07-09 20:26:55 +0100

    new project (ﾉ･o･)ﾉ

---
+++ lib/ecto_jsonb.ex
@@ -0,0 +1,18 @@
+defmodule EctoJsonb do
+  @moduledoc """
+  Documentation for EctoJsonb.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoJsonb.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_jsonb_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoJsonbTest do
+  use ExUnit.Case
+  doctest EctoJsonb
+
+  test "greets the world" do
+    assert EctoJsonb.hello() == :world
+  end
+end
