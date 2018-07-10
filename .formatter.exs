ea36ac5f605c4c94508fb492ecd2f4941456d667
ontofractal <ontofractal@protonmail.com>
2018-07-10 12:33:38 +0100

    add ecto formatting rules to .formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,43 @@
 # Used by "mix format"
+locals_without_parens = [
+  # Query
+  from: 2,
+  from: 1,
+  plug: 1,
+  plug: 2,
+
+  # Schema
+  field: 1,
+  field: 2,
+  field: 3,
+  timestamps: 0,
+  timestamps: 1,
+  belongs_to: 2,
+  belongs_to: 3,
+  has_one: 2,
+  has_one: 3,
+  has_many: 2,
+  has_many: 3,
+  many_to_many: 2,
+  many_to_many: 3,
+  embeds_one: 2,
+  embeds_one: 3,
+  embeds_one: 4,
+  embeds_many: 2,
+  embeds_many: 3,
+  embeds_many: 4,
+
+  # Migrations
+  add: 3,
+  add: 2,
+  execute: 1
+]
+
 [
+  locals_without_parens: locals_without_parens,
+  export: [
+    locals_without_parens: locals_without_parens
+  ],
+  line_length: 80,
   inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
