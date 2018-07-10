c1c99cf34bea01f56517ae90405fdc65703fa678
ontofractal <ontofractal@protonmail.com>
2018-07-10 12:19:49 +0100

    setup testing environment on test run

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,33 @@
 ExUnit.start()
+
+alias Ecto.Integration.TestRepo
+
+Application.put_env(
+  :ecto,
+  TestRepo,
+  adapter: Ecto.Adapters.Postgres,
+  url: "ecto://postgres:postgres@localhost/ecto_pg_json_test",
+  pool: Ecto.Adapters.SQL.Sandbox
+)
+
+defmodule Ecto.Integration.TestRepo do
+  use Ecto.Repo, otp_app: :ecto
+end
+
+defmodule EctoPgJson.TestCase do
+  use ExUnit
+
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(TestRepo)
+  end
+end
+
+# Load up the repository, start it, and run migrations
+_ = Ecto.Adapters.Postgres.storage_down(TestRepo.config())
+:ok = Ecto.Adapters.Postgres.storage_up(TestRepo.config())
+
+{:ok, _pid} = TestRepo.start_link()
+
+:ok = Ecto.Migrator.up(TestRepo, 0, TestMigration, log: false)
+Ecto.Adapters.SQL.Sandbox.mode(TestRepo, :manual)
+Process.flag(:trap_exit, true)
