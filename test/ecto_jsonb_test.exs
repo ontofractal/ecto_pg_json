1c6a13f73908e8cbb32ece5b45eba3fda6369aea
ontofractal <ontofractal@protonmail.com>
2018-07-10 12:55:53 +0100

    add test for get_text macro

--- test/ecto_jsonb_test.exs
+++ test/ecto_jsonb_test.exs
@@ -1,8 +1,16 @@
 defmodule EctoJsonbTest do
-  use ExUnit.Case
-  doctest EctoJsonb
+  use EctoPgJson.TestCase
 
-  test "greets the world" do
-    assert EctoJsonb.hello() == :world
+  import EctoJsonb
+  import Ecto.Query
+  @table "metrics"
+
+  test "get_text macro" do
+    q =
+      from t in @table,
+        where: get_text(t.attributes, "data") == "nope",
+        select: t.id
+
+    assert TestRepo.one(q) == 1
   end
 end
