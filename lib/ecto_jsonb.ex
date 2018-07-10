b607f6687215a1668aa713e8430be1dcb4d202f6
ontofractal <ontofractal@protonmail.com>
2018-07-10 13:35:54 +0100

    add docs for all functions

--- lib/ecto_jsonb.ex
+++ lib/ecto_jsonb.ex
@@ -6,13 +6,9 @@ defmodule EctoJsonb do
   """
 
   @doc """
-  Hello world.
+  get_text_in corresponds to the #>> Postgres operator
 
-  ## Examples
-
-      iex> EctoJsonb.hello
-      :world
-
+  Accepts jsonb column as a first argument and a path string or a list of strings as a second argument.
   """
   defmacro get_text_in(jsonb, path) when is_binary(path) do
     quote do
@@ -20,6 +16,11 @@ defmodule EctoJsonb do
     end
   end
 
+  @doc """
+  get_text_in corresponds to the #>> Postgres operator.
+
+  Accepts jsonb column as a first argument and a path string or a list of strings as a second argument.
+  """
   defmacro get_text_in(jsonb, path) when is_list(path) do
     path = "{" <> Enum.join(path, ",") <> "}"
 
@@ -28,12 +29,22 @@ defmodule EctoJsonb do
     end
   end
 
+  @doc """
+  get_object_in corresponds to the #> Postgres operator.
+
+  Accepts jsonb column as a first argument and a path string or a list of strings as a second argument.
+  """
   defmacro get_object_in(jsonb, path) when is_binary(path) do
     quote do
       fragment("? #> ?", unquote(jsonb), unquote(path))
     end
   end
 
+  @doc """
+  get_object_in corresponds to the #> Postgres operator.
+
+  Accepts jsonb column as a first argument and a path string or a list of strings as a second argument.
+  """
   defmacro get_object_in(jsonb, path) when is_list(path) do
     path = "{" <> Enum.join(path, ",") <> "}"
 
@@ -42,42 +53,77 @@ defmodule EctoJsonb do
     end
   end
 
+  @doc """
+  get_object corresponds to the -> Postgres operator.
+
+  Accepts jsonb column as a first argument and a field or index as a second argument.
+  """
   defmacro get_object(jsonb, field_or_index) do
     quote do
       fragment("? -> ?", unquote(jsonb), unquote(field_or_index))
     end
   end
 
+  @doc """
+  get_text corresponds to the ->> Postgres operator.
+
+  Accepts jsonb column as a first argument and a field or index as a second argument.
+  """
   defmacro get_text(jsonb, field_or_index) do
     quote do
       fragment("? ->> ?", unquote(jsonb), unquote(field_or_index))
     end
   end
 
+  @doc """
+  left_cointains? corresponds to the @> Postgres operator.
+
+  Accepts jsonb columns as a first and second argument
+  """
   defmacro left_contains?(left_jsonb, right_jsonb) do
     quote do
       fragment("? @> ?", unquote(left_jsonb), unquote(right_jsonb))
     end
   end
 
+  @doc """
+  right_contains? corresponds to the <@ Postgres operator.
+
+  Accepts jsonb columns as a first and second argument
+  """
   defmacro right_contains?(left_jsonb, right_jsonb) do
     quote do
       fragment("? <@ ?", unquote(left_jsonb), unquote(right_jsonb))
     end
   end
 
+  @doc """
+  keys_exist? corresponds to the ? Postgres operator.
+
+  Accepts jsonb columns as a first and a string as a second argument
+  """
   defmacro keys_exist?(jsonb, string) when is_binary(string) do
     quote do
       fragment("? ? ?", unquote(jsonb), "?", unquote(string))
     end
   end
 
+  @doc """
+  keys_exist?(jsonb, any: strings) corresponds to the ?| Postgres operator.
+
+  Accepts jsonb columns as a first and an [any: strings] keyword list as a second argument
+  """
   defmacro keys_exist?(jsonb, any: strings) when is_list(strings) do
     quote do
       fragment("? ?| ?", unquote(jsonb), unquote(strings))
     end
   end
 
+  @doc """
+  keys_exist?(jsonb, all: strings) corresponds to the ?& Postgres operator.
+
+  Accepts jsonb columns as a first and an [all: strings] keyword list as a second argument
+  """
   defmacro keys_exist?(jsonb, all: strings) when is_list(strings) do
     quote do
       fragment("? ?& ?", unquote(jsonb), unquote(strings))
