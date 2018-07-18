049e7b37433c989c6eb29b2efe3f8f52b003a58f
ontofractal <ontofractal@protonmail.com>
2018-07-18 21:50:06 +0100

    improve code readability

--- lib/ecto_pg_json_experimental.ex
+++ lib/ecto_pg_json_experimental.ex
@@ -7,69 +7,51 @@ defmodule EctoPgJson.Experimental do
 
   defmacro json_get(json, key_or_index)
            when is_binary(key_or_index) or is_integer(key_or_index) do
-    quote do
-      fragment("? -> ?", unquote(json), unquote(key_or_index))
-    end
+    quote do: fragment("? -> ?", unquote(json), unquote(key_or_index))
   end
 
   defmacro json_get(json, path) when is_list(path) do
     path = build_path_from_list(path)
 
-    quote do
-      fragment("? #> ?", unquote(json), unquote(path))
-    end
+    quote do: fragment("? #> ?", unquote(json), unquote(path))
   end
 
   defmacro json_get(json, key, :text) when is_binary(key) do
-    quote do
-      fragment("? ->> ?", unquote(json), unquote(key))
-    end
+    quote do: fragment("? ->> ?", unquote(json), unquote(key))
   end
 
   defmacro json_get(json, index, :text) when is_binary(index) do
-    quote do
-      fragment("? ->> ?", unquote(json), unquote(index))
-    end
+    quote do: fragment("? ->> ?", unquote(json), unquote(index))
   end
 
   defmacro json_get(json, path, :text) when is_list(path) do
     path = "{" <> Enum.join(path, ",") <> "}"
 
-    quote do
-      fragment("? #>> ?", unquote(json), unquote(path))
-    end
+    quote do: fragment("? #>> ?", unquote(json), unquote(path))
   end
 
   defmacro json_get(json, path, :float) when is_list(path) do
     path = build_path_from_list(path)
 
-    quote do
-      fragment("(? #>> ?)::FLOAT", unquote(json), unquote(path))
-    end
+    quote do: fragment("(? #>> ?)::FLOAT", unquote(json), unquote(path))
   end
 
   defmacro json_get(json, path, :integer) when is_list(path) do
     path = build_path_from_list(path)
 
-    quote do
-      fragment("(? #>> ?)::INTEGER", unquote(json), unquote(path))
-    end
+    quote do: fragment("(? #>> ?)::INTEGER", unquote(json), unquote(path))
   end
 
   defmacro json_get(json, path, :decimal) when is_list(path) do
     path = build_path_from_list(path)
 
-    quote do
-      fragment("(? #>> ?)::DECIMAL", unquote(json), unquote(path))
-    end
+    quote do: fragment("(? #>> ?)::DECIMAL", unquote(json), unquote(path))
   end
 
   defmacro json_get(json, path, :numeric) when is_list(path) do
     path = build_path_from_list(path)
 
-    quote do
-      fragment("(? #>> ?)::NUMERIC", unquote(json), unquote(path))
-    end
+    quote do: fragment("(? #>> ?)::NUMERIC", unquote(json), unquote(path))
   end
 
   def build_path_from_list(args) when is_list(args) do
